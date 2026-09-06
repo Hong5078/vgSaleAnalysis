cf42cad58c55d6d3593a3187422e3f76cfb6af7f
Hong5078 <misaka2022@gmail.com>
2026-09-06 01:29:18 -0400

    First code implementation
    First code implementation

--- paper_replication/replicate_paper.py
+++ paper_replication/replicate_paper.py
@@ -21,7 +21,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-RANDOM_STATE = 42
+RANDOM_STATE = 5078
 CSV_PATH = "video_game_sales_and_ratings.csv"
 
 # ---------------------------------------------------------------------------
